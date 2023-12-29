845d88623489e9d18d56b3fcbb59fc6399770afb
JaccobStanton <jstanton23@my.stlcc.edu>
2023-12-29 11:22:35 -0600

    Added the <DroneLogs/> component to the react router

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ function App() {
               <Route path="/date-mission" element={<Calendar />} />
               <Route path="/plan-mission" element={<Mission_Planner />} />
               <Route path="/live_stats" element={<LiveStats />} />
+              {/* <Route path="/drone-logs" element={<DroneLogs />} /> */}
             </Routes>
           </main>
         </div>
