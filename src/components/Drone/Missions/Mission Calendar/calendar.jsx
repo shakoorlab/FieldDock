07fcdcf39f97968c44b3081b03d56c45fbf096ed
JaccobStanton <jstanton23@my.stlcc.edu>
2024-01-08 12:36:06 -0600

    Removed un-needed components and imports

--- src/components/Drone/Missions/Mission Calendar/calendar.jsx
+++ src/components/Drone/Missions/Mission Calendar/calendar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import multiMonthPlugin from "@fullcalendar/multimonth";
 import interactionPlugin from "@fullcalendar/interaction";
 import "./calendar.css";
 import CommonRow from "../../../NavBar/NavBar";
@@ -9,6 +10,22 @@ import { Container, Row, Col } from "react-bootstrap";
 import { CalendarButton } from "../../../Tools/Buttons/Calendar/calendarButton";
 
 function Calendar() {
+  const handleDateSelect = (selectInfo) => {
+    // You can access start and end date of the selection from selectInfo object
+    let title = prompt("Please enter the time:");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
   return (
     <Container fluid>
       <div>
@@ -25,29 +42,38 @@ function Calendar() {
       </Row>
       <div
         style={{
-          width: "94%",
+          width: "100%",
           marginLeft: "auto",
           marginRight: "auto",
           border: "1px solid #797979",
+          borderRadius: "5px",
           boxShadow: "3px 3px 6px 0 rgba(0, 0, 0, 0.65",
         }}
       >
         <Fullcalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          plugins={[
+            dayGridPlugin,
+            timeGridPlugin,
+            interactionPlugin,
+            multiMonthPlugin,
+          ]}
+          selectable={true}
+          select={handleDateSelect}
           initialView={"dayGridMonth"}
           headerToolbar={{
-            start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+            start: "prev,next", // will normally be on the left. if RTL, will be on the right
             center: "title",
-            end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+            end: "dayGridMonth", // will normally be on the right. if RTL, will be on the left
           }}
           height={"90vh"}
+          width={"100%"}
         />
       </div>
       <Row>
         {/* Content for the fourth row */}
         <Col className="center-content">
           <CalendarButton text="Back" />
-          <CalendarButton text="Save" />
+          <CalendarButton text="Edit" />
           <CalendarButton text="Next" />
         </Col>
       </Row>
